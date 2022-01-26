9f510e789fe23df6dc2c0de87750380efeea062c
krissykodes <kristinscholten@gmail.com>
2022-01-26 12:17:19 -0800

    census troubleshooting

---
+++ notebooks/config.py
@@ -0,0 +1,7 @@
+census_key= "e48a8339d00963d6c0b809c550d138e6c5e766bb"
+# OpenWeatherMap API Key
+weather_api_key = "cfb5b8b764ac7eb15d90dc48f2ef0c33"
+
+# Google API Key
+g_key = "AIzaSyCTi-59eMdoXkIfy_NIfAgNzkMJIKvT9cM"
+
